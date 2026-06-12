f8cdb8a7b75d131322aa1c81334d9ea6052b1d2e
Luswepo Silumbwe <luswepo17@gmail.com>
2026-06-12 09:52:08 +0200

    Updated URLs

--- utils/api_constants.ts
+++ utils/api_constants.ts
@@ -4,19 +4,19 @@
 
 
 // //local
-export const base_ip = "localhost:2000";
-export const next_url = "localhost:3000";
-export const base_url = `http://${base_ip}/v1`;
+// export const base_ip = "localhost:2000";
+// export const next_url = "localhost:3000";
+// export const base_url = `http://${base_ip}/v1`;
 
-export const sandbox_url = `http://${base_ip}`;
+// export const sandbox_url = `http://${base_ip}`;
 
 
 //prod
-// export const base_ip = "api.sandbox.mygeepay.com"; 
-// export const next_url = "api.sandbox.mygeepay.com";
-// export const base_url = `https://${base_ip}/v1`;
+export const base_ip = "api.sandbox.mygeepay.com"; 
+export const next_url = "api.sandbox.mygeepay.com";
+export const base_url = `https://${base_ip}/v1`;
 
-// export const sandbox_url = `https://${base_ip}`;
+export const sandbox_url = `https://${base_ip}`;
 
 // API Endpoints
 export const api_endpoints = {
